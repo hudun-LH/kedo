e72a2a376e1e54e28dba980b0c5bc77e0d336268
walkor <worker-man@qq.com>
2014-05-31 15:19:31 +0800

    注册

--- applications/Todpole/Views/reg.tpl.php
+++ applications/Todpole/Views/reg.tpl.php
@@ -11,7 +11,7 @@
 <div class="container">
 	<div class="row clearfix">
 		<div class="col-md-4 column">
-			<form class="form-horizontal" role="form">
+			<form class="form-horizontal" role="form"  action="/regsave.php" method="post">
 				<div class="form-group">
 					 <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
 					<div class="col-sm-10">
@@ -34,7 +34,7 @@
 				</div>
 				<div class="form-group">
 					<div class="col-sm-offset-2 col-sm-10">
-						 <button type="submit" class="btn btn-default">Sign in</button>
+						 <button type="submit" class="btn btn-default">注册</button>
 					</div>
 				</div>
 			</form>
